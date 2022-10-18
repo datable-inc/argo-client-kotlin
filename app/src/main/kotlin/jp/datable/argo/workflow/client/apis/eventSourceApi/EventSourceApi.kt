c10f5ae954b78ae809e578ecadc641d848ccb44f
sampo02 <m.nishitsuji@gmail.com>
2022-10-18 15:55:38 +0900

    implement getEventSources by openapi generated models

--- app/src/main/kotlin/jp/datable/argo/workflow/client/apis/eventSourceApi/EventSourceApi.kt
+++ app/src/main/kotlin/jp/datable/argo/workflow/client/apis/eventSourceApi/EventSourceApi.kt
@@ -5,7 +5,7 @@ import com.github.michaelbull.result.andThen
 import jp.datable.argo.workflow.client.apis.BaseApi
 import jp.datable.argo.workflow.client.https.HttpClient
 import jp.datable.argo.workflow.client.https.HttpResponse
-import jp.datable.argo.workflow.client.models.EventSources
+import jp.datable.argo.workflow.client.models.EvEventSourceList
 import okhttp3.Call
 import org.springframework.http.HttpMethod
 
@@ -15,32 +15,27 @@ class EventSourceApi(httpClient: HttpClient) : BaseApi(httpClient) {
     }
 
     /** GET /api/v1/event-sources/{namespace} */
-    fun getEventSources(
-        namespace: String,
-    ): Result<HttpResponse<EventSources>, Exception> {
-        return getEventSourcesWithHttpInfo(namespace)
+    fun getEventSources(): Result<HttpResponse<EvEventSourceList>, Exception> {
+        return getEventSourcesWithHttpInfo()
     }
 
-    private fun getEventSourcesCall(namespace: String): Result<Call, Exception> {
-        val path = buildEventSourcesPath(namespace)
+    private fun getEventSourcesCall(): Result<Call, Exception> {
+        val path = buildEventSourcesPath()
         return httpClient.buildCall(path, HttpMethod.GET, null, null)
     }
 
-    private fun getEventSourcesWithHttpInfo(
-        namespace: String,
-    ): Result<HttpResponse<EventSources>, Exception> {
-        return getEventSourcesCall(namespace).andThen {
-            httpClient.execute(it, EventSources::class.java)
+    private fun getEventSourcesWithHttpInfo(): Result<HttpResponse<EvEventSourceList>, Exception> {
+        return getEventSourcesCall().andThen {
+            httpClient.execute(it, EvEventSourceList::class.java)
         }
     }
 
-    private fun buildEventSourcesPath(namespace: String): String {
+    private fun buildEventSourcesPath(): String {
         return StringBuilder()
             .also {
                 it.append(BASE_PATH)
                 it.append(EVENT_SOURCES_PATH)
                 it.append("/")
-                it.append(namespace)
             }
             .toString()
     }
