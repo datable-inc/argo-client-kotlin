2e66700683cdbcde34362889129eafc995227575
sampo02 <m.nishitsuji@gmail.com>
2022-10-19 13:44:01 +0900

    add updateEventSource endpoint test

--- app/src/test/kotlin/jp/datable/argo/workflow/client/apis/eventSourceApi/EventSourceApiTest.kt
+++ app/src/test/kotlin/jp/datable/argo/workflow/client/apis/eventSourceApi/EventSourceApiTest.kt
@@ -5,6 +5,9 @@ import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Ok
 import jp.datable.argo.workflow.client.https.HttpClient
 import jp.datable.argo.workflow.client.https.HttpHeader
+import jp.datable.argo.workflow.client.models.EvEventSource
+import jp.datable.argo.workflow.client.models.EventsourceUpdateEventSourceRequest
+import jp.datable.argo.workflow.client.models.MetaObjectMeta
 import org.junit.Ignore
 import org.junit.Test
 
@@ -17,7 +20,58 @@ internal class EventSourceApiTest {
 
         val httpClient =
             HttpClient.connect(setting.host, HttpHeader(mapOf("Authorization" to setting.bearerToken)))
-        when (val result = EventSourceApi(httpClient).getEventSources()) {
+        when (val result = EventSourceApi(httpClient).getEventSources(setting.eventSourceNamespace)) {
+            is Ok -> {
+                println(result.value)
+            }
+            is Err -> {
+                println(result.error)
+            }
+        }
+    }
+
+    @Test
+    fun updateEventSource() {
+        val setting = ConnectionSetting.get()
+
+        val httpClient =
+            HttpClient.connect(setting.host, HttpHeader(mapOf("Authorization" to setting.bearerToken)))
+
+        val eventSources =
+            when (
+                val result =
+                    EventSourceApi(httpClient).getEventSources(setting.eventSourceNamespace)
+            ) {
+                is Ok -> result.value
+                is Err -> throw Exception(result.error)
+            }
+
+        val resourceVersion =
+            eventSources.data.items
+                ?.find { item -> item.metadata?.name == setting.eventSourceName }
+                ?.metadata
+                ?.resourceVersion
+
+        val body =
+            EventsourceUpdateEventSourceRequest(
+                eventSource =
+                EvEventSource(
+                    metadata =
+                    MetaObjectMeta(
+                        name = setting.eventSourceName,
+                        namespace = setting.eventSourceNamespace,
+                        resourceVersion = resourceVersion,
+                    ),
+                ),
+                name = setting.eventSourceName,
+                namespace = setting.eventSourceNamespace
+            )
+
+        when (
+            val result =
+                EventSourceApi(httpClient)
+                    .updateEventSource(setting.eventSourceNamespace, setting.eventSourceName, body)
+        ) {
             is Ok -> {
                 println(result.value)
             }
